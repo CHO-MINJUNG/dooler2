483c6f7cbcaae0d92447b6f88f363df15a208957
yeojingi <businessjingi@gmail.com>
2022-01-25 23:08:02 +0900

    Revert "main Page setup"
    This reverts commit e3593815c8111f88d34985b215465e96b717c655.

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import ContentListMain from './content_list/ContentListMain';
-import Header from '../components/Header';
-import {Container} from '@mui/material';
-import MainIntroBoard from './content_list/MainIntroBoard';
 
 const Main = () => {
     return (
-        <Container fixed>
-            <Header></Header>
-            <MainIntroBoard></MainIntroBoard>
-            <ContentListMain></ContentListMain>
-        </Container>
+        <div>
+            메인 페이지
+        </div>
     );
 };
 
