730b63822fc7939441e3bd53fd2f704e25ed5790
yeojingi <businessjingi@gmail.com>
2022-01-25 23:09:05 +0900

    Revert "error App.js"
    This reverts commit bd428787a472a7878620448112daf8e36709fd59.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import  { Main }  from './pages';
-import ContentSingle from "./pages/ContentSingle";
+import  {Main}  from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact={true} element={<Main/>} /> 
-        <Route path="/:no" exact={true} element={<ContentSingle/>} /> 
+        <Route path="/:no" exact={true} element={<Main/>} /> 
       </Routes>
     </BrowserRouter>
   );
