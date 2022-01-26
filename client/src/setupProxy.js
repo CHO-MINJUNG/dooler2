c40dfd04902dd27392e583090bdecb6e65758778
yeojingi <businessjingi@gmail.com>
2022-01-26 19:06:07 +0900

    proxy magic number fixed

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,15 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
+const serverDomainUrl = 'http://localhost';
+const PORT = 5000;
+
+const serverUrl = serverDomainUrl+':'+PORT;
 
 module.exports = function (app) {
+
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:5000',
+            target: serverUrl,
             changeOrigin: true
         })
     );
