b028cbb51022e477bdaa7bfad234d52ba56e3478
MINJUNG-CHO <61917990+CHO-MINJUNG@users.noreply.github.com>
2022-01-26 16:06:43 +0900

    db column명 수정

--- client/src/pages/content_list/Thumbnail.js
+++ client/src/pages/content_list/Thumbnail.js
@@ -6,7 +6,7 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 class Thumbnail extends Component {
     render() {
         const item = this.props.item;
-        const title = item.office_title.substring(0, 19);
+        const title = item.title.substring(0, 19);
 
         // if (title.length > 19) {
         //     title = item.title.substring(0, 19);
@@ -15,18 +15,18 @@ class Thumbnail extends Component {
 
         return (
             <div>
-                <a href={"/" + this.props.item.office_info_id} style={{textDecoration: 'none', color: 'black'}}>
-                <ImageListItem key={item.thumbnail}>
+                <a href={"/" + this.props.item.id} style={{textDecoration: 'none', color: 'black'}}>
+                <ImageListItem key={item.img}>
                     <img
-                        src={`${item.thumbnail}?w=248&fit=crop&auto=format`}
-                        srcSet={`${item.thumbnail}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                        alt={item.office_title}
+                        src={`${item.img}?w=248&fit=crop&auto=format`}
+                        srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                        alt={item.title}
                         loading="lazy"
                         style={{width:"247px",height:"172px"}}
                     />
                     <ImageListItemBar
                         title={title}
-                        subtitle={<span>{item.office_location}</span>}
+                        subtitle={<span>{item.location}</span>}
                         position="below"
                     />
                 </ImageListItem>
--- server/routes/main.js
+++ server/routes/main.js
@@ -20,14 +20,12 @@ router.get('/', (req,res) => {
 router.get('/:id', (req,res) => {
     const id = req.params.id;
     connection.query(
-        `select 
-            office_info_id, 
-            thumbnail, 
-            office_title, 
-            office_location 
-        from Office_Info
-        where office_info_id = ?`,
-        id,
+        `select image_id ,
+        office_info_id id,
+        image_data
+        from Office_Image
+        `,
+        // where office_info_id = ?id,
         (err,rows,field) => {
             console.log(rows)
             res.send(rows);
@@ -35,4 +33,10 @@ router.get('/:id', (req,res) => {
     )
 })
 
+// select 
+//             office_info_id as id, 
+//             thumbnail as img, 
+//             office_title as title, 
+//             office_location as location
+//         from Office_Info 
 module.exports = router;
