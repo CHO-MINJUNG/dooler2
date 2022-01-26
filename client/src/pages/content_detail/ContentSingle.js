6373fdda9d0edbed53fc5f7a9fdd8b64fa82c2a0
yeojingi <businessjingi@gmail.com>
2022-01-26 21:21:58 +0900

    fetch is made in list and signle

--- client/src/pages/content_detail/ContentSingle.js
+++ client/src/pages/content_detail/ContentSingle.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import Header from '../../components/Header';
+import React, {Component, useState, useEffect} from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 import {Container} from '@mui/material';
+
+import Header from '../../components/Header';
 
 import office from '../../assets/office1.jpeg';
 import OfficeImageContainer from './OfficeImageContainer';
@@ -9,14 +12,35 @@ import OfficeInfoContainer from './OfficeInfoContainer';
 import './style.css';
 
 const ContentSingle = () => {
-    return (
-        <Container fixed>
-            <Header></Header>
-            <h1>하나의 게시물입니다.</h1>
-            <OfficeImageContainer image={office}></OfficeImageContainer>
-            <OfficeInfoContainer></OfficeInfoContainer>
-        </Container>
-    );
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const param = useParams();
+
+	const callApi = async () => {
+		const id = param.id;
+		
+		try {
+			const response = await axios.get(`/api/${id}`);
+			console.log(response.data);
+			setData(response.data);
+		} catch(err) {
+			console.log("Error >>", err);
+		}
+	}
+
+  useEffect(() => {
+    callApi();
+  }, loading);
+
+
+	return (
+		<Container fixed>
+			<Header></Header>
+			<h1>하나의 게시물입니다.</h1>
+			<OfficeImageContainer image={office}></OfficeImageContainer>
+			<OfficeInfoContainer></OfficeInfoContainer>
+		</Container>
+	);
 };
 
 export default ContentSingle;
--- client/src/pages/content_list/Thumbnail.js
+++ client/src/pages/content_list/Thumbnail.js
@@ -6,7 +6,11 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 class Thumbnail extends Component {
     render() {
         const item = this.props.item;
-        const title = item.title.substring(0, 19);
+        console.log(item);
+        const title = item.office_title.substring(0, 19);
+        const imgUrl = item.thumbnail;
+        const location = item.office_location;
+        const id = item.office_info_id;
 
         // if (title.length > 19) {
         //     title = item.title.substring(0, 19);
@@ -17,18 +21,18 @@ class Thumbnail extends Component {
 
         return (
             <div>
-                <a href={"/" + this.props.item.id} style={{textDecoration: 'none', color: 'black'}}>
-                <ImageListItem key={item.img}>
+                <a href={"/" + id} style={{textDecoration: 'none', color: 'black'}}>
+                <ImageListItem key={id}>
                     <img
-                        src={`${item.img}?w=248&fit=crop&auto=format`}
-                        srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                        src={`${imgUrl}`}
+                        srcSet={`${imgUrl}`}
                         alt={item.title}
                         loading="lazy"
                         style={{width:"247px",height:"172px"}}
                     />
                     <ImageListItemBar
                         title={title}
-                        subtitle={<span>{item.location}</span>}
+                        subtitle={<span>{location}</span>}
                         position="below"
                     />
                 </ImageListItem>
