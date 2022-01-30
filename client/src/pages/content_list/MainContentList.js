ba4c8bdf8d6384f9310020a19abf77f588385c87
yeojingi <businessjingi@gmail.com>
2022-01-31 01:24:14 +0900

    main long banner added
    long banner added

--- client/src/pages/content_list/MainContentList.js
+++ client/src/pages/content_list/MainContentList.js
@@ -2,14 +2,15 @@ import React, { useEffect } from 'react';
 import ContentListMain from './ContentListMain';
 import Header from '../../components/Header';
 import {Container, Divider} from '@mui/material';
-import MainIntroBoard from './MainIntroBoard';
+import MainIntroBoard from './LongBanner';
+import LongBanner from './LongBanner';
 
 const MainContentList = () => {
     return (
         <Container fixed maxWidth="md">
             <Header></Header>
             <Divider></Divider>
-            {/* <MainIntroBoard></MainIntroBoard> */}
+            <LongBanner></LongBanner>
             <ContentListMain></ContentListMain>
         </Container>
     );
