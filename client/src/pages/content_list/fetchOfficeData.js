49c09be192817fbfcf502e47a0d539d7d155a38c
MINJUNG-CHO <61917990+CHO-MINJUNG@users.noreply.github.com>
2022-02-02 23:28:24 +0900

    api 설정 틀렸음 ㅠㅠ

--- client/src/pages/content_list/fetchOfficeData.js
+++ client/src/pages/content_list/fetchOfficeData.js
@@ -4,7 +4,7 @@ export const API_BASE_URL = process.env.REACT_APP_API_ROOT;
 
 const fetchOfficeData = async (setData) => {
   try {
-    const response = await axios.get('/api')
+    const response = await axios.get(`${API_BASE_URL}/api`)
     setData(response.data)
   } catch(err) {
     console.log("Error >>", err);
