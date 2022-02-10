8327d867a85f20ada11fb15ae7a249ab1ff55ba1
yeojingi <businessjingi@gmail.com>
2022-02-10 17:21:31 +0900

    글쓰기 최종완성
    응

--- client/src/pages/editor/FormContainer.js
+++ client/src/pages/editor/FormContainer.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import contentReducer from './contentSlice';
 import ImageUploader from "./ImageUploader";
+import ThumnbnailEditor from "./ThumbnailEditor";
 
 const FormContainer = () => {
 
