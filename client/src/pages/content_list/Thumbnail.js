d4abaab1189129c9030ca34db4798f3e443000dd
yeojingi <businessjingi@gmail.com>
2022-02-18 23:02:36 +0900

    new sign 추가

--- client/src/pages/content_list/Thumbnail.js
+++ client/src/pages/content_list/Thumbnail.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
-import { Typography } from '@mui/material';
+import { Chip, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import { getDateReadable } from './getDateReadable';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import { Icon } from '@mui/material';
+import { Stack } from '@mui/material';
+
+import newSign from '../../assets/new_sign.png';
 
 export const API_BASE_URL = process.env.REACT_APP_API_ROOT;
 
@@ -20,6 +25,10 @@ class Thumbnail extends Component {
 		const create_time = item.create_time;
 
 		const domain = `${API_BASE_URL}/api/crop_image/`;
+
+		const color1 = '';
+		const color2 = '#cccccc';
+		const color3 = '#7b7b7b';
         
 		return (
 			<Grid item key={item} xs={12} sm={6} md={4} sx={
@@ -30,7 +39,7 @@ class Thumbnail extends Component {
 				}}}>
 				<a href={'dreams/' + id} style={{textDecoration: 'none'}}>
 					<Card
-						sx={{ height: '100%', display: 'flex', flexDirection: 'column'}}
+						sx={{ position: 'relative', height: '100%', display: 'flex', flexDirection: 'column'}}
 					>
 						<CardMedia
 							component="img"
@@ -38,27 +47,42 @@ class Thumbnail extends Component {
 							image={item.thumbnail}
 							alt="사무실 사진"
 						/>
+						{this.getNewSign(create_time)}
 						<CardContent sx={{ flexGrow: 1}}>
-							<Typography fontWeight={500} noWrap={true} >
+						<Typography fontSize={20} fontWeight={700}>
+								월 {fee}
+							</Typography>
+							<Typography sx={{fontSize: 14}} fontWeight={350}>
+								{location}
+							</Typography>
+							<Typography noWrap={true} sx={{color: color2, fontSize: 14, marginTop: '5px'}} fontWeight={350}>
 								{title}
 							</Typography>
-							<Grid container direction={"row"} justifyContent={'space-between'}>
-								<Typography sx={{fontSize: 13, marginTop: '5px'}} fontWeight={350} color="text.secondary">
-									{location}
-								</Typography>
-								<Typography sx={{fontSize: 15, marginTop: '5px'}} fontWeight={600}>
-									{fee}
-								</Typography>
-							</Grid>
-							<Grid container justifyContent={'end'} spacing={1}>
+							<Grid container sx={{marginTop: '5px'}} justifyContent={'space-between'} spacing={1} >
 								<Grid item>
-									<Typography sx={{fontSize: 13, marginTop: '5px'}} fontWeight={350} color="text.secondary">
+									<Typography sx={{fontSize: 13, color: color3}} fontWeight={350} color="text.secondary">
 										{getDateReadable(create_time)}
 									</Typography>
 								</Grid>
-								<Grid item>
-									<Typography sx={{fontSize: 13, marginTop: '5px'}} fontWeight={350} color="text.secondary">
-										조회수: {views_count}
+								<Grid item sx={{
+									display: 'flex',
+									alignItems: 'center',
+									flexWrap: 'wrap',}}
+								>
+									{/* <Chip icon={<VisibilityIcon sx={{fontSize: 13,}}/>} label={'h'}></Chip> */}
+									<VisibilityIcon sx={{ fontSize: 13, color: color3}}  />
+									<Typography
+										variant="span"
+										sx={{
+											color: color3, 
+											fontSize: 13, 
+											display: 'inline-block', 
+											marginLeft: '2px'
+										}} 
+										fontWeight={200}
+										color="text.secondary"
+									>
+										{views_count}
 									</Typography>
 								</Grid>
 							</Grid>
@@ -68,6 +92,35 @@ class Thumbnail extends Component {
 			</Grid>
 		);
   }
+
+	getNewSign (timestamp) {
+		var date = new Date(timestamp);
+	
+		const now = new Date(Date.now());
+	
+		var difference = now - date;
+	
+		var daysDifference = Math.floor(difference/1000/60/60/24);
+	
+		var readable = '';
+		console.log(daysDifference);
+		if (daysDifference < 2) {
+			return (
+				<img 
+					src={newSign} 
+					style={{
+						position:'absolute',
+						width: '60px',
+						top: '5px',
+						left: '5px',
+					}}
+				/>
+			);
+		}  else {
+			return (<div></div>);
+		}
+	
+	}
 };
 
 export default Thumbnail;
