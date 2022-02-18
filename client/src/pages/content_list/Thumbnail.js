4cfecbde25c63e6068d2092f5b7988b6b606d69c
yeojingi <businessjingi@gmail.com>
2022-02-18 18:02:33 +0900

    3열로 복원

--- client/src/pages/content_list/Thumbnail.js
+++ client/src/pages/content_list/Thumbnail.js
@@ -24,25 +24,32 @@ class Thumbnail extends Component {
 		return (
 			<Grid item key={item} xs={12} sm={6} md={4} sx={
 				{
-					width: "100%",
 					'&:hover': {  
 					transitionDuration: '500ms',
 					transform:'scale(1.1)',
 				}}}>
 				<a href={'dreams/' + id} style={{textDecoration: 'none'}}>
 					<Card
-						sx={{ height: '100%', display: 'flex',}}
+						sx={{ height: '100%', display: 'flex', flexDirection: 'column'}}
 					>
 						<CardMedia
 							component="img"
-							sx={{ width: '40%', height: '200px'}}
+							height={192}
 							image={item.thumbnail}
 							alt="사무실 사진"
 						/>
 						<CardContent sx={{ flexGrow: 1}}>
-							<Typography fontWeight={500} sx={{fontSize: 15, marginBottom: '5%'}} >
+							<Typography fontWeight={500} noWrap={true} >
 								{title}
 							</Typography>
+							<Grid container direction={"row"} justifyContent={'space-between'}>
+								<Typography sx={{fontSize: 13, marginTop: '5px'}} fontWeight={350} color="text.secondary">
+									{location}
+								</Typography>
+								<Typography sx={{fontSize: 15, marginTop: '5px'}} fontWeight={600}>
+									{fee}
+								</Typography>
+							</Grid>
 							<Grid container justifyContent={'end'} spacing={1}>
 								<Grid item>
 									<Typography sx={{fontSize: 13, marginTop: '5px'}} fontWeight={350} color="text.secondary">
@@ -54,14 +61,6 @@ class Thumbnail extends Component {
 										조회수: {views_count}
 									</Typography>
 								</Grid>
-							</Grid>
-							<Grid container direction={"row"} justifyContent={'space-between'}>
-								<Typography sx={{fontSize: 18, marginTop: '5px'}} fontWeight={350} color="text.secondary">
-									{location}
-								</Typography>
-								<Typography sx={{fontSize: 18, marginTop: '5px'}} fontWeight={600}>
-									{fee}
-								</Typography>
 							</Grid>
 						</CardContent>
 					</Card>
