b44eec180340255de7de46a5329eaeea6498304f
yeojingi <businessjingi@gmail.com>
2022-03-07 13:57:10 +0900

    paging 시도.. (진기)

--- client/components/list/fetchOfficeData.js
+++ client/components/list/fetchOfficeData.js
@@ -5,12 +5,14 @@ axios.defaults.withCredentials=true;
 export const API_BASE_URL = process.env.NEXT_PUBLIC_REACT_APP_API_ROOT;
 
 const fetchOfficeData = async (setData, page) => {
+  console.log(`API_BASE_URL: ${API_BASE_URL}`);
   try {
     axios({
       method:'get',
-      url: `${API_BASE_URL}/api?page=${page}`})  
+      url: `/api?page=${page}`})
       .then(response => {
-        setData(response.data)});
+        setData(response.data)
+        console.log(`fetchOfficeData.js ${response.data}`)});
   } catch(err) {
     console.log("Error >>", err);
   }
